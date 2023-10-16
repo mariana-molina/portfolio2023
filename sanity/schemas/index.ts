56a53a23f9dde6e9eb29146413deea091a0c6dd7
Mariana <marsmolina20@gmail.com>
2023-10-16 13:49:50 +0200

    add sanity and schemas

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,7 @@
+import experience from './experience'
+import pageInfo from './pageInfo'
+import project from './project'
+import skills from './skills'
+import social from './social'
+
+export const schemaTypes = [pageInfo, experience, skills, project, social]
---
+++ sanity/schemas/skills.ts
@@ -0,0 +1,30 @@
+import {defineType} from 'sanity'
+
+export default defineType({
+  name: 'skills',
+  title: 'Skills',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      description: 'Title of the Skill',
+      type: 'string',
+    },
+    {
+      name: 'progress',
+      title: 'Progress',
+      description: 'Progress of skill from 0 to 100%',
+      type: 'number',
+      validation: (Rule) => Rule.min(0).max(100),
+    },
+    {
+      name: 'imgage',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+})
